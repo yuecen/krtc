584d2180ddc36d78e814a10da4987cc0da804bc2
shpwang <shpwang@users.noreply.github.com>
2015-07-15 18:02:32 +0800

    change FB comments language  from en_US to zh_TW

--- router.js
+++ router.js
@@ -34,7 +34,7 @@ myapp.controller('newsCtrl', function($scope){
            var js, fjs = d.getElementsByTagName(s)[0];
             //if (d.getElementById(id)) return;
             js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_us/sdk.js#xfbml=1&version=v2.4";
+            js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.4";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
     });
